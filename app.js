1276b7388142bfa9507393e212c29b6e53469da9
Mkhitaryan Ivan <mkhit@NB-01-IvanM.si.corp.adacta-group.com>
2018-10-11 21:25:25 +0300

    finish whith loops module

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const grammarAndTypes = require('./lib/grammar-and-types')
 const controlFlowErrorHandling = require('./lib/control-flow-error-handling')
 const loops = require('./lib/loops')
+
+loops.loops()
