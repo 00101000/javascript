4566e2b2d2aeb5df2a26a073002a4f770f82b4ca
Mkhitaryan Ivan <mkhit@NB-01-IvanM.si.corp.adacta-group.com>
2018-10-13 11:50:26 +0300

    add operators module

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const grammarAndTypes = require('./lib/grammar-and-types')
 const controlFlowErrorHandling = require('./lib/control-flow-error-handling')
 const loops = require('./lib/loops')
 const fucntions = require('./lib/functions')
+const expressionsAndOperators = require('./lib/expressions-and-operartors')
 
-fucntions.predefinedFunctionsUdage()
+expressionsAndOperators.inOperator()
