91d9c02b136cd7d43d318ffbac871d7928dd83c4
Mkhitaryan Ivan <mkhit@NB-01-IvanM.si.corp.adacta-group.com>
2018-10-13 15:01:38 +0300

    finish dates and numbers

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const controlFlowErrorHandling = require('./lib/control-flow-error-handling')
 const loops = require('./lib/loops')
 const fucntions = require('./lib/functions')
 const expressionsAndOperators = require('./lib/expressions-and-operartors')
+const numbersAndDates = require('./lib/numbers-and-dates')
 
-expressionsAndOperators.inOperator()
+numbersAndDates.dateObjectUsage()
